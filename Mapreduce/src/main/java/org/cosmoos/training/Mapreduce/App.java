0ff3cdcb8ab82f5d9f25c10b57942672f749b64c
surajn03 <suraj.nagarcenkar@gmail.com>
2016-05-24 23:05:53 -0700

    Inital version
    Change-Id: I9d055bdbbf7f4d0fbe467185a4ff3de78168aa43

---
+++ Mapreduce/src/main/java/org/cosmoos/training/Mapreduce/App.java
@@ -0,0 +1,13 @@
+package org.cosmoos.training.Mapreduce;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
